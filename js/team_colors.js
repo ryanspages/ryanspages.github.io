58487d9a2bc6a30e117e89d498488f30e2733d32
plaintextspokane <133164421+plaintextspokane@users.noreply.github.com>
2025-12-19 09:20:07 -0800

    Update team_colors.js
    update Arizona name in colors and switch some team colors around

--- js/team_colors.js
+++ js/team_colors.js
@@ -1,7 +1,7 @@
 // js/team_colors.js
 
 const TEAM_COLORS = {
-  "ARZ": ["#A71930", "#E3D4AD", "#000000"], // Sedona red, Sonoran sand, black
+  "ARI": ["#A71930", "#E3D4AD", "#000000"], // Sedona red, Sonoran sand, black
   "ATL": ["#CE1141", "#13274F", "#EAAA00"], // scarlet, navy, gold
   "BAL": ["#DF4601", "#000000", "#ffffff"], // orange, black, white
   "BOS": ["#BD3039", "#0C2340", "#ffffff"], // red, blue, white
@@ -9,27 +9,27 @@ const TEAM_COLORS = {
   "CHW": ["#27251F", "#C4CED4", "#ffffff"], // black, silver, white
   "CIN": ["#C6011F", "#000000", "#ffffff"], // red, black, white
   "CLE": ["#00385D", "#E50022", "#ffffff"], // blue, red, white
-  "COL": ["#333366", "#C4CED4", "#131413"], // Rockies purple, silver, midnight black
+  "COL": ["#333366", "#131413", "#C4CED4"], // Rockies purple, midnight black, silver
   "DET": ["#0C2340", "#FA4616", "#ffffff"], // navy, orange, white
   "HOU": ["#002D62", "#EB6E1F", "#F4911E"], // navy, orange, light orange
   "KCR": ["#004687", "#BD9B60", "#ffffff"], // Royal blue, gold, white
   "LAA": ["#003263", "#BA0021", "#ffffff"], // midnight blue, red, white
   "LAD": ["#005A9C", "#EF3E42", "#A5ACAF"], // Dodger blue, red, silver
   "MIA": ["#00A3E0", "#EF3340", "#41748D"], // Miami blue, Caliente red, Slate gray
-  "MIL": ["#ffc52f", "#12284b", "#ffffff"], // yellow, navy blue, white
-  "MIN": ["#002B5C", "#D31145", "#B9975B"], // Twins navy, scarlet red, Kasota gold
+  "MIL": ["#12284b", "#ffffff", "#ffc52f"], // navy blue, white, yellow
+  "MIN": ["#002B5C", "#B9975B", "#D31145"], // Twins navy, Kasota gold, scarlet red 
   "NYM": ["#002D72", "#FF5910", "#ffffff"], // blue, orange, white
   "NYY": ["#0C2340", "#C4CED3", "#ffffff"], // Navy, gray, white
-  "ATH": ["#003831", "#EFB21E", "#a2aaad"], // green, gold, gray
+  "ATH": ["#003831", "#a2aaad", "#EFB21E"], // green, gray, gold
   "PHI": ["#E81828", "#002d72", "#FFFFFF"], // red, blue, white
-  "PIT": ["#27251F", "#FDB827", "#ffffff"], // Process black, gold, white
+  "PIT": ["#27251F", "#ffffff", "#FDB827"], // Process black, white, gold
   "SDP": ["#2F241D", "#FFC425", "#ffffff"], // Padres brown, Padres gold, white
   "SFG": ["#FD5A1E", "#27251F", "#ffffff"], // orange, black, beige
   "SEA": ["#0C2C56", "#005C5C", "#C4CED4"], // navy blue, northwest green, silver
   "STL": ["#C41E3A", "#0C2340", "#ffffff"], // Cardinal red, navy, white
-  "TBR": ["#092C5C", "#8FBCE6", "#F5D130"], // Navy blue, Columbia blue, yellow
+  "TBR": ["#092C5C", "#F5D130", "#8FBCE6"], // Navy blue, yellow, Columbia blue
   "TEX": ["#003278", "#C0111F", "#ffffff"], // blue, red, white
-  "TOR": ["#134A8E", "#1D2D5C", "#E8291C"], // blue, navy blue, red
+  "TOR": ["#134A8E", "#1D2D5C", "#ffffff"], // blue, navy blue, white
   "WSH": ["#AB0003", "#14225A", "#ffffff"], // red, navy blue, white
   
 
